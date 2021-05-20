2a108212d648a900fcb3667f37be71f81e817b79
alex <74060189+alexredk@users.noreply.github.com>
2021-05-20 23:32:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,143 @@
+// let i = prompt('Введите число')
+// if(i > 0) {
+//     i++ 
+//     console.log(i)
+// }
+// else{
+//     console.log(i)
+// }
+// let i = prompt('ВВедите целое число')
+// if(i > 0) {
+//     i++
+//     console.log(i)
+// }
+// else{
+//     i -= 2
+//     console.log(i)
+// }
+// let i = prompt('ВВедите целое число')
+// if(i > 0){
+//     i++
+//     console.log(i)
+// }
+// else if(i < 0){
+//     i -=2
+//     console.log(i)
+// }
+// else{
+//     i +=10
+//     console.log(i)
+// }
+// let a = prompt('ВВедите числа')
+//     b = prompt('ВВедите число')
+//     с = prompt('Введите число')
+// if (a > 0 && b > 0 && c > 0){
+//     alert('Три положительных числа' )
+// }
+// else if(a > 0 && b > 0 && c < 0 ){
+//     alert('Два числа положительных')
+// }
+
+// else if(a < 0 && b > 0 && c > 0 ){
+//     alert('Два числа положительных')
+// }
+// else if(a < 0 && b < 0 && c > 0){
+//     alert('Одно число положительное')
+// }
+// else if(a <0 && b > 0 && c < 0){
+//     alert('Одно число ')
+// }
+// else if(a > 0 && b < 0 && c < 0){
+//     alert('Одно число')
+// }
+// else{
+//     alert('Нет чисел')
+// }
+// let a = prompt('ВВедите числа')
+//     b = prompt('ВВедите число')
+//     с = prompt('Введите число')
+// if (a < 0 && b < 0 && c < 0){
+//     alert('Тпм негативных числа' )
+// }
+// else if(a < 0 && b > 0 && c < 0 ){
+//     alert('Два числа негативных')
+// }
+
+// else if(a < 0 && b < 0 && c > 0 ){
+//     alert('Два числа негативных')
+// }
+// else if(a > 0 && b < 0 && c < 0){
+//     alert('Одно число негативное')
+// }
+// else if(a > 0 && b < 0 && c > 0){
+//     alert('Одно число негативное')
+// }
+// else if(a < 0 && b > 0 && c < 0){
+//     alert('Одно число')
+// }
+// else{
+//     alert('Нет чисел')
+// let i = prompt('Вывести большее из них')
+// let a = prompt('Введите число')
+// if(i > a){
+//     alert('Первое число больше')
+// }
+// if(a > i){
+//     alert('Второе число больше')
+// }
+// let a = prompt('Введите число')
+// let b = prompt('Введите число')
+// if(a > b){
+//     console.log(a)
+// }
+// if(a < b){
+//     console.log(b)
+// }
+// let a = prompt('Введите число')
+// let b = prompt('введите число')
+// if(a > b){
+//     console.log(a)
+//     console.log(b)
+
+// }
+// if(a < b){
+//     console.log(b)
+//     console.log(a)
+// }
+// let a = prompt('ВВедите число')
+// let b = prompt('Введите число')
+// let c = prompt('Введите число')
+// if(a < b < c){
+//     alert('Первое число меньше всех')
+// }
+// else if(a > b < c) {
+//     alert('Второе число меньше всех')
+// }
+// else(a > b > c) {
+//     alert('Третье число самое меньшее')
+// }
+// let a = prompt(' ')
+// let b = prompt(' ')
+// let c = prompt(' ')
+// if(a > b , a > c){
+//     alert(a)
+// }
+// else if(b > c , b > a){
+//     alert(b)
+// }
+// else(c > b , c < a){
+//     alert(c)
+// }
+
+// let a = prompt('Введите число ')
+// let b = prompt(' Введите число')
+// let c = prompt('Введите число')
+// if(a + b < a + c)(
+//     alert('Сумма первых двух чисел юольше')
+// )
+// if(b + c < b + a){
+//     alert("Сумма b + a больше")
+// }
+// if(c + a < c + b){
+//     alert('Сумма c + b больше')
+// }
